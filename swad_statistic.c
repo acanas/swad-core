74e19c92aa7ba4885b2a300c7ff7a9910c514c49
Antonio Cañas Vargas <acanas@ugr.es>
2015-03-09 00:35:30 +0100

    Version 14.80.6

--- swad_statistic.c
+++ swad_statistic.c
@@ -7206,6 +7206,18 @@ static void Sta_GetAndShowNumUsrsPerSideColumns (void)
             sprintf (Query,"SELECT COUNT(*) FROM usr_data WHERE SideCols='%u'",
                      SideCols);
             break;
+	 case Sco_SCOPE_CTY:
+            sprintf (Query,"SELECT COUNT(DISTINCT usr_data.UsrCod)"
+        	           " FROM institutions,centres,degrees,courses,crs_usr,usr_data"
+                           " WHERE institutions.CtyCod='%ld'"
+                           " AND institutions.InsCod=centres.InsCod"
+                           " AND centres.CtrCod=degrees.CtrCod"
+                           " AND degrees.DegCod=courses.DegCod"
+                           " AND courses.CrsCod=crs_usr.CrsCod"
+                           " AND crs_usr.UsrCod=usr_data.UsrCod"
+                           " AND usr_data.SideCols='%u'",
+                     Gbl.CurrentCty.Cty.CtyCod,SideCols);
+            break;
 	 case Sco_SCOPE_INS:
             sprintf (Query,"SELECT COUNT(DISTINCT usr_data.UsrCod)"
         	           " FROM centres,degrees,courses,crs_usr,usr_data"
