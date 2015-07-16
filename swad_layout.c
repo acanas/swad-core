3c9f057109cfab23064a14cc769cba87dc5e39cd
Antonio Cañas Vargas <acanas@ugr.es>
2015-07-17 00:52:35 +0200

    Version 14.117.3

--- swad_layout.c
+++ swad_layout.c
@@ -130,9 +130,7 @@ void Lay_WriteStartOfPage (void)
        Gbl.CurrentAct == ActRefLstClk)
      // Don't generate a full HTML page, only refresh connected users
      {
-      fprintf (Gbl.F.Out,"Content-Type: text/html; charset=windows-1252\r\n");
-      // fprintf (Gbl.F.Out,"Cache-Control: max-age=0, no-cache, must-revalidate\r\n\r\n");
-
+      fprintf (Gbl.F.Out,"Content-Type: text/html; charset=windows-1252\r\n\r\n");
       Gbl.Layout.WritingHTMLStart = false;
       Gbl.Layout.HTMLStartWritten = Gbl.Layout.TablEndWritten = true;
       return;
