34aad33da1dfda347f6e98afe155c110178a07f6
Antonio Cañas Vargas <acanas@ugr.es>
2015-12-08 22:41:12 +0100

    Version 15.60.7

--- swad_changelog.h
+++ swad_changelog.h
@@ -111,7 +111,7 @@
 /*****************************************************************************/
 
 #define Log_PLATFORM_VERSION	"SWAD 15.60.7 (2015/12/08)"
-#define CSS_FILE		"swad15.59.4.css"
+#define CSS_FILE		"swad15.60.7.css"
 
 // Number of lines (includes comments but not blank lines) has been got with the following command:
 // nl swad*.c swad*.h css/swad*.css py/swad*.py js/swad*.js soap/swad*.h sql/swad*.sql | tail -1
