b9903ab2da642e67ce3431a3636a66b3479e1f79
Antonio Cañas Vargas <acanas@ugr.es>
2016-11-18 21:47:24 +0100

    Version 16.64.6

--- swad_changelog.h
+++ swad_changelog.h
@@ -168,13 +168,14 @@
 /****************************** Public constants *****************************/
 /*****************************************************************************/
 
-#define Log_PLATFORM_VERSION	"SWAD 16.64.5 (2016-11-17)"
+#define Log_PLATFORM_VERSION	"SWAD 16.64.6 (2016-11-17)"
 #define CSS_FILE		"swad16.60.1.css"
 #define JS_FILE			"swad16.46.1.js"
 
 // Number of lines (includes comments but not blank lines) has been got with the following command:
 // nl swad*.c swad*.h css/swad*.css py/swad*.py js/swad*.js soap/swad*?.h sql/swad*.sql | tail -1
 /*
+        Version 16.64.6:  Nov 17, 2016	Input fields required in log in. (207270 lines)
         Version 16.64.5:  Nov 17, 2016	Input fields required in record card. (207268 lines)
         Version 16.64.4:  Nov 17, 2016	Contextual help on guests and administrators. (207264 lines)
         Version 16.64.3:  Nov 17, 2016	Contextual help on teachers. (207259 lines)
