b7d1f173280a88e009855eb76947f56c756a2954
Antonio Cañas Vargas <acanas@ugr.es>
2016-12-16 10:35:13 +0100

    Version 16.103.1

--- swad_changelog.h
+++ swad_changelog.h
@@ -188,13 +188,14 @@
 /****************************** Public constants *****************************/
 /*****************************************************************************/
 
-#define Log_PLATFORM_VERSION	"SWAD 16.103 (2016-12-16)"
-#define CSS_FILE		"swad16.97.css"
+#define Log_PLATFORM_VERSION	"SWAD 16.103.1 (2016-12-16)"
+#define CSS_FILE		"swad16.103.1.css"
 #define JS_FILE			"swad16.101.js"
 
 // Number of lines (includes comments but not blank lines) has been got with the following command:
 // nl swad*.c swad*.h css/swad*.css py/swad*.py js/swad*.js soap/swad*?.h sql/swad*.sql | tail -1
 /*
+        Version 16.103.1: Dec 16, 2016	Changes in layout of tabs. (211123 lines)
         Version 16.103:   Dec 16, 2016	Contextual help on course info. (211130 lines)
         Version 16.102:   Dec 15, 2016	Fixed bug in mail to students, reported by GitHub user raistmaj.
 					Fixed bug in file of marks, reported by GitHub user raistmaj.
