a4d1c71d9d8d35bc384e2171ba6ac3bddeef2d60
Antonio Cañas Vargas <acanas@ugr.es>
2018-11-09 08:45:29 +0100

    Version 18.13.5

--- swad_changelog.h
+++ swad_changelog.h
@@ -359,7 +359,7 @@ ps2pdf source.ps destination.pdf
 #define CSS_FILE		"swad18.4.css"
 #define JS_FILE			"swad17.17.1.js"
 /*
-	Version 18.13.5:  Nov 08, 2018  New file extension allowed, requested by Javier Fernández Baldomero: .gdb (GNU Debugger Script). (? lines)
+	Version 18.13.5:  Nov 08, 2018  New file extension allowed, requested by Javier Fernández Baldomero: .gdb (GNU Debugger Script). (236537 lines)
 	Version 18.13.4:  Nov 05, 2018  Fixed bug in listing of users.
 					Fixed bug when logging comments.
 					Code refactoring in queries. (236535 lines)
