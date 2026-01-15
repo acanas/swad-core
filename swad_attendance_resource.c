c1a60b09cb5caa61d0842a76786191964d8caeac
acanas <acanas@ugr.es>
2026-01-15 18:31:22 +0100

    Version 25.35:    Jan 15, 2026	Changes to install swad in Ubuntu 24.04.

--- swad_attendance_resource.c
+++ swad_attendance_resource.c
@@ -5,7 +5,7 @@
     and used to support university teaching.
 
     This file is part of SWAD core.
-    Copyright (C) 1999-2025 Antonio Cañas Vargas
+    Copyright (C) 1999-2026 Antonio Cañas Vargas
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
